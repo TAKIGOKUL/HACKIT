41133f476c2ce4701ea743c42edcdaa328a6255d
TAKIGOKUL <gokulgkblueheart@gmail.com>
2024-02-29 13:14:30 +0530

    updated token

--- login/update.js
+++ login/update.js
@@ -1,40 +1,41 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBzGLohu4cQVTdB9kBCGPFN3AVnT5eyeHI",
-    authDomain: "fir-eba48.firebaseapp.com",
-    projectId: "fir-eba48",
-    storageBucket: "fir-eba48.appspot.com",
-    messagingSenderId: "316656891183",
-    appId: "1:316656891183:web:19c80a73275d7551b41dda",
-    measurementId: "G-XCS92351S3"
+  apiKey: "AIzaSyBzGLohu4cQVTdB9kBCGPFN3AVnT5eyeHI",
+  authDomain: "fir-eba48.firebaseapp.com",
+  projectId: "fir-eba48",
+  storageBucket: "fir-eba48.appspot.com",
+  messagingSenderId: "316656891183",
+  appId: "1:316656891183:web:19c80a73275d7551b41dda",
+  measurementId: "G-XCS92351S3",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-auth.languageCode = 'en';
+auth.languageCode = "en";
 const provider = new GoogleAuthProvider();
 
 const user = auth.currentUser;
 
 // After login user details can be taken as shown below, ID are not working replace it according to our patient page okay.
 function updateUserProfile(user) {
-    const userName = user.displayName;
-    const userEmail = user.email;
-    const userProfilePicture = user.photoURL;
+  const userName = user.displayName;
+  const userEmail = user.email;
+  const userProfilePicture = user.photoURL;
 
-    document.getElementById("userName").textContent = userName;
-    document.getElementById("userEmail").textContent = userEmail;
-    document.getElementById("userProfilePicture").src = userProfilePicture;
+  document.getElementById("userName").textContent = userName;
+  document.getElementById("userEmail").textContent = userEmail;
+  document.getElementById("userProfilePicture").src = userProfilePicture;
 }
-onAuthStateChanged(auth,(user)=>{
-    if(user){
-        updateUserProfile(user);
-        const uid = user.uid;
-        return uid;
-    }else{
-        alert("Create account and login..!!");
-        window.location.href = "/login_page.html"
-    }
-})
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    updateUserProfile(user);
+    const uid = user.uid;
+    return uid;
+  }
+});
